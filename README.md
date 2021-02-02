a4742eab37fc4216d04104459e54de0b7609d309
Rainmain <810061281@qq.com>
2021-02-02 09:16:17 +0000

    job

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Kubernetes-Monitor
+
+# 1 Prometheus Job 问题
+1. prometheus configmap中，node-exporter的地址怎么填写？因为每个主机上都有node-exporter的pod，这个pod的地址是不断的变化的，应该怎么填写这个地址？？？
+2. 为什么同样的配置文件，就可以监控promtheus而不可以监控node-exporter？？？是不是因为这个job是在promtheus的容器中执行的，可以通过prometheus容器的localhost找到自己？？？
+```yaml
+- job_name: 'prometheus'
+    static_configs:
+    - targets:
+        - "localhost:9090"
+
+- job_name: 'kubernetes-node-exporter'
+    static_configs:
+    - targets:
+    - "localhost:9100"
+```
+3. kubernetes-node 和 node-exporter之间的联系和区别是什么？
+4. 为什么监控到的 pods 和 service 是处于down状态的？
--- up.sh
+++ up.sh
@@ -2,38 +2,54 @@
 
 # start up running network
 function networkUp() {
+	# node-exporter
     kubectl create -f ./node-exporter.yaml
-	# sudo kubectl apply -f ./prometheus
-	kubectl create -f ./prometheus/blackbox-exporter.yaml
-	kubectl create -f ./prometheus/rbac-setup.yaml
-	kubectl create -f ./prometheus/configmap.yaml
-	kubectl create -f ./prometheus/prometheus.svc.yml
-	kubectl create -f ./prometheus/prometheus.deploy.yml
-	# sudo kubectl apply -f ./grafana
-	kubectl create -f ./grafana/grafana-svc.yaml
-	kubectl create -f ./grafana/grafana-ing.yaml
-	kubectl create -f ./grafana/grafana-volume.yaml
-	kubectl create -f ./grafana/grafana-deploy.yaml
-	kubectl apply -f ./kube-state-metrics
+
+	# kube-state-metrics
+	kubectl create -f ./kube-state-metrics
+
+	# prometheus 
+	kubectl create -f ./prometheus
+	# kubectl create -f ./prometheus/blackbox-exporter.yaml
+	# kubectl create -f ./prometheus/rbac-setup.yaml
+	# kubectl create -f ./prometheus/configmap.yaml
+	# kubectl create -f ./prometheus/prometheus.svc.yml
+	# kubectl create -f ./prometheus/prometheus.deploy.yml
+
+	# grafana
+	kubectl create -f ./grafana
+	# kubectl create -f ./grafana/grafana-svc.yaml
+	# kubectl create -f ./grafana/grafana-ing.yaml
+	# kubectl create -f ./grafana/grafana-volume.yaml
+	# kubectl create -f ./grafana/grafana-deploy.yaml
+	
 
 }
 
 
 # Tear down running network
 function networkDown() {
+	# node-exporter
     kubectl delete -f ./node-exporter.yaml
-	# sudo kubectl delete -f ./prometheus
-	kubectl delete -f ./prometheus/blackbox-exporter.yaml
-	kubectl delete -f ./prometheus/rbac-setup.yaml
-	kubectl delete -f ./prometheus/configmap.yaml
-	kubectl delete -f ./prometheus/prometheus.deploy.yml
-	kubectl delete -f ./prometheus/prometheus.svc.yml
-	# sudo kubectl delete -f ./grafana
-	kubectl delete -f ./grafana/grafana-svc.yaml
-	kubectl delete -f ./grafana/grafana-ing.yaml
-	kubectl delete -f ./grafana/grafana-volume.yaml
-	kubectl delete -f ./grafana/grafana-deploy.yaml
+	
+	# kube-state-metrics
 	kubectl delete -f ./kube-state-metrics
+	
+	# prometheus
+	kubectl delete -f ./prometheus
+	# kubectl delete -f ./prometheus/blackbox-exporter.yaml
+	# kubectl delete -f ./prometheus/rbac-setup.yaml
+	# kubectl delete -f ./prometheus/configmap.yaml
+	# kubectl delete -f ./prometheus/prometheus.deploy.yml
+	# kubectl delete -f ./prometheus/prometheus.svc.yml
+
+
+	# grafana
+	kubectl delete -f ./grafana
+	# kubectl delete -f ./grafana/grafana-svc.yaml
+	# kubectl delete -f ./grafana/grafana-ing.yaml
+	# kubectl delete -f ./grafana/grafana-volume.yaml
+	# kubectl delete -f ./grafana/grafana-deploy.yaml
 }
 
 function networkRestart() {
